103dd650a23e4996e4a082cd392c7e8dd76045a7
vlaskinmac <kangelina86@mail.ru>
2021-12-14 17:24:46 +0300

    - Adjusted the logic rebuild() - Created pagination in index.html - Removed second container from index.html - Removed indexes from index.html - Added urlencode in links index.html - Added second loop in index.html

--- render_website.py
+++ render_website.py
@@ -1,7 +1,5 @@
-import itertools
 import json
 import os
-from pprint import pprint
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server, shell
 from more_itertools import chunked
@@ -12,16 +10,21 @@ def rebuild():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html'])
     )
-    template = env.get_template('index.html')
+    template = env.get_template('pages/index.html')
     with open('filejson.json') as json_file:
         book_collection = json.load(json_file)
-        books_divided = list(chunked(book_collection, 10))
+    pages = list(chunked(book_collection, 10))
     os.makedirs('pages', exist_ok=True)
-    for number, book in enumerate(books_divided, 1):
-        # books_divided = list(chunked(book, 2))
-        rendered_page = template.render(books_divided=book)
+    for number, page in enumerate(pages, 1):
+        books_divided = list(chunked(page, 2))
+        rendered_page = template.render(
+            books_divided=books_divided,
+            current_page=number,
+            last_page=len(pages),
+        )
         with open(f'pages/index{number}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
+
 
 rebuild()
 server = Server()
