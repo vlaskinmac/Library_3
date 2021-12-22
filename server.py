6eeb96d83d9381ed44ddeab7e439e0541bbc9e6b
vlaskinmac <kangelina86@mail.ru>
2021-12-22 13:17:37 +0300

    Added if/main in server

--- server.py
+++ server.py
@@ -2,8 +2,9 @@ from livereload import Server
 
 from render_website import rebuild
 
-rebuild()
 
-server = Server()
-server.watch('Library_3/*.html', rebuild)
-server.serve(root='.')
+if __name__ == "__main__":
+    rebuild()
+    server = Server()
+    server.watch('Library_3/*.html', rebuild)
+    server.serve(root='.')
